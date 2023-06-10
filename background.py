77a6d911b01a474aeea968926d5dd13d2b949d1d
trupax8 <maceo.iacobozzi@gmail.com>
2023-06-10 10:44:33 +0200

    fixed buttons and reorganised files

--- background.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from settings import display
-
-def background(backgroundleft, backgroundright):
-    # Define the position of the background image for the left side in x-axis and y-axis
-    display.blit(backgroundleft, (0, 0))
-    # Define the position of the background image for the right side in x-axis and y-axis
-    display.blit(backgroundright, (700, 0))
--- text.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-from settings import black
-
-def text_object(text, font):
-    # Set color of the message
-    text_surface = font.render(text, True, black)
-    # After that, restart the game and ready to give some input
-    return text_surface, text_surface.get_rect()
